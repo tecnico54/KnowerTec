2c3d29f6d3dbd69b1f541eef0656a72134d27a43
tecnico54 <115370228+tecnico54@users.noreply.github.com>
2025-01-29 07:53:40 -0500

    Initial commit
    Knowledge

---
+++ JavaScript/Recomendación-imagen-2.js
@@ -0,0 +1,4 @@
+const contenedor = document.getElementById('Tarjetas-Madre');
+contenedor.addEventListener('click', () => {
+    contenedor.classList.toggle('Tarjetas-Madre-pc'); // Alterna la clase para el efecto
+});
---
+++ JavaScript/Recomendación-imagen.js
@@ -0,0 +1,4 @@
+const imagen = document.getElementById('Shopping-Pc');
+    imagen.addEventListener('click', () => {
+    imagen.classList.toggle('Shopping'); // Añade o quita la clase "ampliada" al hacer clic
+});
